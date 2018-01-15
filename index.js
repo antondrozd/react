50f5f1117893476042a7a65152eae11b4471e54e
Anton Drozd <anton.drozd@uneedweb.com>
2018-01-15 14:13:20 +0200

    extend additional info

--- index.js
+++ index.js
@@ -3,36 +3,62 @@ const root = document.getElementById('root');
 let contactObjCounter = 0;
 
 class ContactObj {
-	constructor(name, phone, imgsrc) {
+	constructor(name, phone, imgsrc, adress, email) {
 		this.id = ++contactObjCounter;
 		this.name = name;
 		this.phone = phone;
 		this.image = imgsrc;
+		this.adress = adress;
+		this.email = email;
 	}
 }
 
 const CONTACTS = [
-	new ContactObj('Anton Drozd', '+1234567890', 'img/anton.jpg'),
-	new ContactObj('Kirill Buga', '+1234567890', 'img/kirill.jpg'),
-	new ContactObj('Diana Buga', '+1234567890', 'img/diana.jpg'),
-	new ContactObj('Anastasia Ushakova', '+1234567890', 'img/nastya.jpg'),
-	new ContactObj('Kateryna Alekseyenko', '+1234567890', 'img/katya.jpg'),
-	new ContactObj('Mark Getman', '+1234567890', 'img/mark.jpg'),
-	new ContactObj('Bohdan Kushpler', '+1234567890', 'img/bohdan.jpg'),
-	new ContactObj('Iryna Sliusarenko', '+1234567890', 'img/ira.jpg')
+	new ContactObj('Kirill Buga', '+1234567890', 'img/kirill.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Diana Buga', '+1234567890', 'img/diana.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Anton Drozd', '+1234567890', 'img/anton.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Anastasia Ushakova', '+1234567890', 'img/nastya.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Kateryna Alekseyenko', '+1234567890', 'img/katya.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Mark Getman', '+1234567890', 'img/mark.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Bohdan Kushpler', '+1234567890', 'img/bohdan.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com'),
+	new ContactObj('Iryna Sliusarenko', '+1234567890', 'img/ira.jpg', 'Bazhana Ave 24/1', 'me@uneedweb.com')
 ]
 
 
 class Contact extends React.Component {
+	state = {
+		isOpened: false
+	}
+
+	handleAdditionalInfo = () => {
+		if (this.state.isOpened) {
+			this.setState({
+				isOpened: false
+			})} else {
+			this.setState({
+				isOpened: true
+			})
+		}
+	}
+
 	render() {
 		const blockName = 'contact-item';
 		return (
-			<li className={blockName}>
+			<li className={blockName} onClick={this.handleAdditionalInfo}>
 				<img className={`${blockName}__photo`} src={this.props.image} />
 				<div className={`${blockName}__info`}>
 					<span className={`${blockName}__name`}>{this.props.name}</span>
 					<span className={`${blockName}__phone`}>{this.props.phone}</span>
+					{
+						this.state.isOpened ? (
+							<div className={`${blockName}__additional-info`}>
+								<span className={`${blockName}__adress`}>{this.props.adress}</span>
+								<span className={`${blockName}__email`}>{this.props.email}</span>
+							</div>
+						) : null
+					}
 				</div>
+				<span className={`${blockName}__state-toggle`}>{this.state.isOpened ? '▲' : '▼'}</span>
 			</li>
 		)
 	}
@@ -64,7 +90,7 @@ class ContactsList extends React.Component {
 				<ul className={`${blockName}__list`}>
 					{
 						this.state.displayedContacts.map(function (el) {
-							return <Contact key={el.id} name={el.name} phone={el.phone} image={el.image} />
+							return <Contact key={el.id} name={el.name} phone={el.phone} image={el.image} adress={el.adress} email={el.email} />
 						})
 					}
 				</ul>
